5bd1d0753fbca9f7a49a2aa3758a8fb457579ab5
Hector <hectorhu17@gmail.com>
2020-02-21 15:16:04 -0300

    adição de exercícios

---
+++ EX1.js
@@ -0,0 +1,15 @@
+/* Crie uma função que recebe dois argumentos string e retorna o de maior comprimento. */
+
+function compare(str1, str2){
+    if (str1.length > str2.length)
+        return str1;
+    else
+        return str2;
+};
+
+/*
+
+let s1 = "Hello", s2 = "world!";
+console.log(compare(s1,s2));
+
+*/
---
+++ EX2.js
@@ -0,0 +1,13 @@
+/* Crie uma função que recebe três argumentos, uma função e duas string, aplique a função nas duas string e imprima o resultado. */
+
+const primeiroArg = function (str1,str2){
+    return (str1 + " " + str2);
+};
+
+function FuncPrincipal(func, str1, str2){
+    return func(str1,str2);
+};
+
+let str = "Olá", str1 = "Mundo";
+
+console.log (FuncPrincipal(primeiroArg, str, str1));
